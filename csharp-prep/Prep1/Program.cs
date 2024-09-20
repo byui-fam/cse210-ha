6c682c76f1bf4ab059f64f1963fead1d87335957
byui-fad <false>
2024-09-20 21:01:40 +0100

    finished prep2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ask for a user name//
         Console.Write("What is your first name? ");
         string First = Console.ReadLine();
 
